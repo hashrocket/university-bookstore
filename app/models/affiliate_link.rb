44ca4dd1043e969a43cb657023eef23d2e26f4f6
Jim Remsik and Vincent Spehner <dev+bigtiger+vzmind@hashrocket.com>
2010-06-02 17:25:31 -0400

    System counts visits to affiliate link
    [finishes #3738152]

--- app/models/affiliate_link.rb
+++ app/models/affiliate_link.rb
@@ -1,5 +1,9 @@
 class AffiliateLink < ActiveRecord::Base
   belongs_to :book
   belongs_to :user
+
+  def increment_visits_count!
+    update_attribute(:visits_count, visits_count.succ)
+  end
 end
 
--- spec/models/affiliate_link_spec.rb
+++ spec/models/affiliate_link_spec.rb
@@ -1,10 +1,16 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe AffiliateLink do
-  subject do
-    Factory.build(:affiliate_link)
-  end
+  let(:affiliate_link) { Factory(:affiliate_link) }
 
   it { should belong_to(:book) }
   it { should belong_to(:user) }
+
+  describe "#increment_visits_count!" do
+    it "increases the visits count by one" do
+      affiliate_link.visits_count.should == 42
+      affiliate_link.increment_visits_count!
+      affiliate_link.visits_count.should == 43
+    end
+  end
 end
